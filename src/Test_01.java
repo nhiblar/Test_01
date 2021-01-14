71f3072e61a9908c428bcfc5640c5f06e54cc183
Nate <natehiblar@gmail.com>
2021-01-13 17:03:58 -0800

    Added Print Line

--- src/Test_01.java
+++ src/Test_01.java
@@ -1,5 +1,6 @@
 public class Test_01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("This is test 1.");
     }
 }
